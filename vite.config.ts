bbb7c3ac3b3dc20078c3eaed83cfca78dc200609
geniee1220 <geniee1220@gmail.com>
2024-02-03 19:26:21 +0900

    Chore : vite-plugin-svgr 라이브러리 설치

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
+import svgr from 'vite-plugin-svgr';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
