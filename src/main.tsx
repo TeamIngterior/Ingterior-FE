372c5857ffd601e9b4ef1615314771467d3337a6
geniee1220 <geniee1220@gmail.com>
2024-02-03 18:52:10 +0900

    Chore :  router v6 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+
+import { RouterProvider } from 'react-router-dom';
+import router from './Router.tsx';
 
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -18,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <App />
+        <RouterProvider router={router} />
       </RecoilRoot>
     </QueryClientProvider>
   </React.StrictMode>
