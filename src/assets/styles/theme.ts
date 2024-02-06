aab563b1b3c8c744c168acb793498c0c7dc34686
geniee1220 <geniee1220@gmail.com>
2024-02-06 15:22:39 +0900

    Design : 전역 색상 변수 설정

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -6,7 +6,18 @@ const layoutComponent = {
 };
 
 const color = {
-  primary: '#3182f6',
+  primary: '#464866',
+  primary100: '#EAEAF1',
+  primary200: '#C9CCD7',
+  primary300: '#A7ABBB',
+  primary400: '#868A9F',
+  primary500: '#6D718B',
+  primary600: '#56597A',
+  secondary: '#ff6347',
+  secondary100: '#FBE8E7',
+  secondary200: '#FFC9BC',
+  secondary300: '#FFA592',
+  secondary400: '#FF8067',
 };
 
 const theme: DefaultTheme = {
