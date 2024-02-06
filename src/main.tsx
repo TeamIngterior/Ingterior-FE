384e3df18eff73e26deb1873adfd6c08ed2b100f
geniee1220 <geniee1220@gmail.com>
2024-02-06 15:05:12 +0900

    Chore : MSW 라이브러리 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 
+// dom
+import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router.tsx';
 
+// recoil, react-query
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+// mocking
+import { worker } from './mocks/browser.ts';
+
+// style
 import ThemeProvider from '@/assets/styles/ThemeProvider.tsx';
+
+if (import.meta.env.MODE === 'development') {
+  worker.start();
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
---
+++ src/mocks/handler.ts
@@ -0,0 +1,3 @@
+import { HttpResponse, http } from 'msw';
+
+export const handlers = [];
