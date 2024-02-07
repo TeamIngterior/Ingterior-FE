828332ba1d58cec6d52463f08cbfea17f32225b1
geniee1220 <geniee1220@gmail.com>
2024-02-07 21:13:08 +0900

    Design : 전역 색상 변수 업데이트

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -7,17 +7,27 @@ const layoutComponent = {
 
 const color = {
   primary: '#464866',
-  primary100: '#EAEAF1',
-  primary200: '#C9CCD7',
-  primary300: '#A7ABBB',
-  primary400: '#868A9F',
-  primary500: '#6D718B',
-  primary600: '#56597A',
-  secondary: '#ff6347',
-  secondary100: '#FBE8E7',
-  secondary200: '#FFC9BC',
-  secondary300: '#FFA592',
-  secondary400: '#FF8067',
+  primary01: '#EAEAF2',
+  primary02: '#CDCFDA',
+  primary03: '#ADB1C0',
+  primary04: '#8E92A5',
+  primary05: '#777A93',
+  primary06: '#616483',
+  primary07: '#464866',
+
+  secondary: '#FF6347',
+  secondary01: '#FBEAE9',
+  secondary02: '#FFCDC0',
+  secondary03: '#FFAB99',
+  secondary04: '#FF8871',
+  secondary05: '#FF6347',
+
+  gray01: '#fff',
+  gray02: '#f4f4f4',
+  gray03: '#ddd',
+  gray04: '#9a9a9a',
+  gray05: '#5d5d5d',
+  gray06: '#1e1e1e',
 };
 
 const theme: DefaultTheme = {
