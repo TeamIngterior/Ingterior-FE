e3751efa67195c6ce25e488f0cdd261536fe6cf9
geniee1220 <geniee1220@gmail.com>
2024-02-11 20:51:22 +0900

    Feat : 소셜 로그인 상수 파일 추가

---
+++ src/constants/socialLogin.tsx
@@ -0,0 +1,50 @@
+import { theme } from '@/assets/styles/theme';
+
+import KakaoLogo from '@assets/socialLogin/logo_kakao.svg?react';
+import NaverLogo from '@assets/socialLogin/logo_naver.svg?react';
+import GoogleLogo from '@assets/socialLogin/logo_google.svg?react';
+import InstagramLogo from '@assets/socialLogin/logo_instagram.svg?react';
+
+export const SOCIAL_LOGIN = [
+  {
+    title: '카카오톡으로 이용하기',
+    $fullWidth: true,
+    icon: <KakaoLogo />,
+    style: {
+      backgroundColor: '#FFEB00',
+      color: `${theme.color.gray06}`,
+      marginBottom: `${theme.gap.gap1}`,
+    },
+  },
+  {
+    title: '네이버로 이용하기',
+    $fullWidth: true,
+    icon: <NaverLogo />,
+    style: {
+      backgroundColor: '#03C75A',
+      color: `${theme.color.gray01}`,
+      marginBottom: `${theme.gap.gap1}`,
+    },
+  },
+  {
+    title: '구글로 이용하기',
+    $fullWidth: true,
+    icon: <GoogleLogo />,
+    style: {
+      backgroundColor: `${theme.color.gray01}`,
+      color: `${theme.color.gray06}`,
+      marginBottom: `${theme.gap.gap1}`,
+      border: `1px solid ${theme.color.gray03}`,
+    },
+  },
+  {
+    title: '인스타그램으로 이용하기',
+    $fullWidth: true,
+    icon: <InstagramLogo />,
+    style: {
+      backgroundColor: `${theme.color.gray01}`,
+      color: `${theme.color.gray06}`,
+      border: `1px solid ${theme.color.gray03}`,
+    },
+  },
+];
