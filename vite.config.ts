fc96fe19fc7d2681dcf34a377cccc7304aad0623
geniee1220 <geniee1220@gmail.com>
2024-02-11 22:15:51 +0900

    Chore : util alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,10 @@ export default defineConfig({
         find: '@assets',
         replacement: path.resolve(__dirname, 'src/assets'),
       },
+      {
+        find: '@utils',
+        replacement: path.resolve(__dirname, 'src/utils'),
+      },
     ],
   },
   server: {
