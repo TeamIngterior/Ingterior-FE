157f8f963fc1de55f88de3f1fc452b86d53a3393
geniee1220 <geniee1220@gmail.com>
2024-02-11 22:16:26 +0900

    Chore : 테스트 파일 경로 지정

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ export default {
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
     '^.+\\.svg$': 'jest-svg-transformer',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
