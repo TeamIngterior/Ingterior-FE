7ae13945f901772df3ba87070f70984b20092a79
geniee1220 <geniee1220@gmail.com>
2024-02-11 18:31:23 +0900

    Chore : 라우터 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,25 @@
 import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import Home from './pages/Home';
+import SignIn from './pages/user/SignIn';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <div>Not Found</div>,
-    children: [],
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <SignIn />,
+      },
+      {
+        path: 'signin',
+        children: [{ index: true, element: <SignIn /> }],
+      },
+    ],
   },
 ]);
 
