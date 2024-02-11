e43b112a0e0a46d8b38d0654817b0a2a7074b846
geniee1220 <geniee1220@gmail.com>
2024-02-12 06:21:20 +0900

    Story : Button 컴포넌트 스토리 작성

---
+++ src/stories/components/Button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from '../../components/common/Button';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    style: { control: 'object' },
+    size: { control: 'radio', options: ['sm', 'md'] },
+    $bgType: { control: 'radio', options: ['default', 'revert'] },
+    $fullWidth: { control: 'boolean', defaultValue: false },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  render: (args) => <Button {...args}>Button</Button>,
+};
