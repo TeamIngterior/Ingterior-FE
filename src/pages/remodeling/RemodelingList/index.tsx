908479acfed8f5eafae6cdfce52ea9a1e4b751c0
geniee1220 <geniee1220@gmail.com>
2024-02-16 01:21:23 +0900

    Feat : 현장 목록 페이지 생성

--- src/pages/remodeling/RemodelingList/index.tsx
+++ src/pages/remodeling/RemodelingList/index.tsx
@@ -1,5 +1,28 @@
+import useMobileHeader from '@/hooks/useMobileHeader';
+
+import { useRemodelingList } from './useRemodelingList';
+import { RemodelingListDataModel } from '@/apis/remodeling';
+
+import RemodelingListCard from '@/components/remodeling/list/RemodlingListCard';
+
+import * as S from './styles';
+
 function RemodelingList() {
-  return <div></div>;
+  useMobileHeader('/', '현장 목록');
+
+  const { remodelingListData } = useRemodelingList();
+
+  return (
+    <>
+      <S.RemodelingListContainer>
+        {remodelingListData?.map(
+          (item: RemodelingListDataModel, index: number) => (
+            <RemodelingListCard cardData={item} key={index} />
+          )
+        )}
+      </S.RemodelingListContainer>
+    </>
+  );
 }
 
 export default RemodelingList;
