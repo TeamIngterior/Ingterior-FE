20f191b872e0e7feffa510aa1beb6df318d3b8f2
geniee1220 <geniee1220@gmail.com>
2024-02-16 03:57:00 +0900

    Refactor : 모바일 헤더를 헤더로 대체

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { useRecoilValue } from 'recoil';
-import { mobileHeaderState } from './atom/globalState';
-
-import MobileHeader from '@components/common/MobileHeader';
+import Header from './components/common/Header';
 import MainTemplate from './components/template/MainTemplate';
 
 function App() {
-  const { prevLink, title } = useRecoilValue(mobileHeaderState);
-
   return (
     <>
-      {prevLink && title && <MobileHeader prevLink={prevLink} title={title} />}
+      <Header />
       <MainTemplate>
         <Suspense fallback={<div>Loading...</div>}>
           <Outlet />
