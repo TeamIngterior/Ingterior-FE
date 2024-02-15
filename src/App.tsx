c883776d19980623ff4da809e96bb5aec0b10884
geniee1220 <geniee1220@gmail.com>
2024-02-15 23:50:48 +0900

    Refactor : 모바일 링크와 타이틀이 있을 때만 타이틀 보이도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+
+import { useRecoilValue } from 'recoil';
+import { mobileHeaderState } from './atom/globalState';
+
+import MobileHeader from '@components/common/MobileHeader';
 import MainTemplate from './components/template/MainTemplate';
 
 function App() {
+  const { prevLink, title } = useRecoilValue(mobileHeaderState);
+
   return (
     <>
+      {prevLink && title && <MobileHeader prevLink={prevLink} title={title} />}
       <MainTemplate>
         <Suspense fallback={<div>Loading...</div>}>
           <Outlet />
