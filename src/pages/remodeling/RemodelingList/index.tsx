b0ac9462f64d49715aac607711452fd7202775e9
geniee1220 <geniee1220@gmail.com>
2024-02-18 09:31:56 +0900

    Feat : 현장 추가 버튼 리스트 추가

--- src/pages/remodeling/RemodelingList/index.tsx
+++ src/pages/remodeling/RemodelingList/index.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { useRemodelingList } from './useRemodelingList';
+
 import { RemodelingListDataModel } from '@/apis/remodeling';
 
+import { FiPlus } from 'react-icons/fi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { MdInput } from 'react-icons/md';
 import RemodelingListCard from '@/components/remodeling/list/RemodlingListCard';
+import Button from '@/components/common/Button';
 
+import { theme } from '@/assets/styles/theme';
 import * as S from './styles';
 
 function RemodelingList() {
   const { remodelingListData } = useRemodelingList();
+  const { ref } = useInView({
+    threshold: 1,
+    onChange: (inView) => {
+      inView
+        ? setIsButtonContainerVisible(true)
+        : setIsButtonContainerVisible(false);
+    },
+  });
+
+  const [isButtonContainerVisible, setIsButtonContainerVisible] =
+    useState(false);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   return (
     <>
@@ -16,6 +36,57 @@ function RemodelingList() {
             <RemodelingListCard cardData={item} key={index} />
           )
         )}
+
+        <div className="constructionButtonContainer" ref={ref}>
+          <S.AddConstructionConatiner
+            style={{
+              position: isButtonContainerVisible ? 'static' : 'fixed',
+            }}
+          >
+            {isButtonClicked && (
+              <S.AddConstructionList>
+                <li>
+                  <Button
+                    type="button"
+                    size="md"
+                    onClickHandler={() => console.log('click')}
+                    style={{
+                      borderColor: `${theme.color.primary05}`,
+                      backgroundColor: `${theme.color.primary05}`,
+                      boxShadow: '0 0 4px 0 rgba(0, 0, 0, 0.4)',
+                    }}
+                  >
+                    <MdInput />
+                    코드로 현장 추가
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    type="button"
+                    size="md"
+                    onClickHandler={() => console.log('click')}
+                    style={{
+                      borderColor: `${theme.color.primary05}`,
+                      backgroundColor: `${theme.color.primary05}`,
+                      boxShadow: '0 0 4px 0 rgba(0, 0, 0, 0.4)',
+                    }}
+                  >
+                    <FiPlus />
+                    <span>새 현장 추가</span>
+                  </Button>
+                </li>
+              </S.AddConstructionList>
+            )}
+            <Button
+              type="button"
+              size="md"
+              onClickHandler={() => setIsButtonClicked(!isButtonClicked)}
+            >
+              {isButtonClicked ? <AiOutlineClose /> : <FiPlus />}
+              현장 추가
+            </Button>
+          </S.AddConstructionConatiner>
+        </div>
       </S.RemodelingListContainer>
     </>
   );
--- src/pages/remodeling/RemodelingList/styles.tsx
+++ src/pages/remodeling/RemodelingList/styles.tsx
@@ -2,5 +2,30 @@ import styled from 'styled-components';
 import { theme } from '@/assets/styles/theme';
 
 export const RemodelingListContainer = styled.div`
+  position: relative;
   padding-top: ${theme.gap.gap1};
+
+  .constructionButtonContainer {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-top: 120px;
+  }
+`;
+export const AddConstructionConatiner = styled.div`
+  bottom: 16px;
+`;
+
+export const AddConstructionList = styled.ul`
+  position: absolute;
+  right: 0;
+  bottom: 64px;
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.gap.gap1};
+
+  li {
+    display: flex;
+    justify-content: flex-end;
+  }
 `;
