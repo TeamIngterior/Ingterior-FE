8084015d7065c6fd2ea69339ccd8e703a1787c37
geniee1220 <geniee1220@gmail.com>
2024-02-19 07:40:10 +0900

    Feat : 현장코드 입장 모달에 코드 입력 버튼 추가

--- src/components/modal/remodeling/AddRemodelingSite/index.tsx
+++ src/components/modal/remodeling/AddRemodelingSite/index.tsx
@@ -2,14 +2,44 @@ import Button from '@/components/common/Button';
 import DefaultModal from '../../DefaultModal';
 
 import * as S from './styles';
+import Input from '@/components/common/Input';
+import { useForm } from 'react-hook-form';
 
 function AddRemodlingSite() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onBlur',
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <DefaultModal name="addRemodelingSite">
       <S.ModalContainer>
         {/* 모달 컨텐츠 */}
         <S.ModalContent>
           <S.ModalHeader>현장 코드를 입력해 주세요.</S.ModalHeader>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              type="text"
+              size="sm"
+              inputType="input"
+              isHorizontal={true}
+              placeholder="공유받은 코드를 입력해 주세요."
+              {...register('siteCode')}
+              errors={errors}
+            >
+              <Button size="sm" $styleType="revert">
+                코드 입력
+              </Button>
+            </Input>
+          </form>
         </S.ModalContent>
 
         {/* 모달 하단 버튼 */}
