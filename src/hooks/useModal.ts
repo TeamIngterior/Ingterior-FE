47e2849c5058948f0c8a3cf17eed81c5d41c8615
geniee1220 <geniee1220@gmail.com>
2024-02-19 21:47:56 +0900

    Feat : 콜백 함수 받도록 useHook 수정

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,22 +1,30 @@
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { modalState } from '@/atom/modalState';
+import { useCallback } from 'react';
 
 export const useModal = (name: string) => {
-  const setAllModalState = useSetRecoilState(modalState);
+  const [allModalState, setAllModalState] = useRecoilState<{
+    [key: string]: any;
+  }>(modalState);
 
-  const openModal = () => {
-    setAllModalState((currentModalState) => ({
-      ...currentModalState,
+  const openModal = useCallback(() => {
+    setAllModalState((prevState) => ({
+      ...prevState,
       [name]: true,
     }));
-  };
+  }, [name, setAllModalState]);
 
-  const closeModal = () => {
-    setAllModalState((currentModalState) => ({
-      ...currentModalState,
-      [name]: false,
-    }));
-  };
+  const closeModal = useCallback(
+    (callback?: () => void) => {
+      setAllModalState((prevState) => ({
+        ...prevState,
+        [name]: false,
+      }));
+      callback && callback();
+    },
+
+    [name, setAllModalState]
+  );
 
   return { openModal, closeModal };
 };
