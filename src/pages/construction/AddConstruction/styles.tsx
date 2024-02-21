eb332531796a6e75bf6ad14e6a9f7e4035300661
geniee1220 <geniee1220@gmail.com>
2024-02-22 03:05:31 +0900

    Feat : 현장 주소, 현장 유형 선택

--- src/pages/construction/AddConstruction/styles.tsx
+++ src/pages/construction/AddConstruction/styles.tsx
@@ -2,3 +2,7 @@ import styled from 'styled-components';
 import { theme } from '@assets/styles/theme';
 
 export const AddConstructionContainer = styled.div``;
+
+export const AddConstructionForm = styled.form`
+  margin-top: ${theme.gap.gap7};
+`;
