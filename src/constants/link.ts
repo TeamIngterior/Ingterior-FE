e793bde0368ec63db50437d9b10260438413ada1
geniee1220 <geniee1220@gmail.com>
2024-02-25 11:22:08 +0900

    Refactor : 링크 path, name을 상수 파일로 분리

---
+++ src/constants/link.ts
@@ -0,0 +1,37 @@
+export const MENU_LINK = [
+  {
+    name: '인테리어 간편견적',
+    path: '',
+  },
+  {
+    name: '인테리어 공사관리',
+    path: '',
+  },
+  {
+    name: '인테리어 TIP',
+    path: '',
+  },
+];
+
+export const SIDE_MENU_LINK = [
+  {
+    name: '인테리어 간편견적',
+    path: '',
+  },
+  {
+    name: '인테리어 공사관리',
+    path: '',
+  },
+  {
+    name: '인테리어 TIP',
+    path: '',
+  },
+  {
+    name: '메세지',
+    path: '/message',
+  },
+  {
+    name: '설정',
+    path: '/setting',
+  },
+];
