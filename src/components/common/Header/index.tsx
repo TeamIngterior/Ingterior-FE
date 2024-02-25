2e1ced583f8297872c8e708d104342220fb9a20e
geniee1220 <geniee1220@gmail.com>
2024-02-25 11:21:48 +0900

    Refactor : Header 컴포넌트 수정 (로그인, 로그인X, 반응형)

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -7,10 +7,12 @@ import MenuIcon from '@assets/icon/menu.svg?react';
 import Sidebar from '../Sidebar';
 
 import * as S from './styles';
+import { MENU_LINK } from '@/constants/link';
 
 function Header() {
   const navigate = useNavigate();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
 
   return (
     <S.HeaderContainer>
@@ -22,11 +24,39 @@ function Header() {
           }}
         />
 
-        {/* 메뉴 버튼 */}
-        <S.MenuContainer onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
+        {/* 메뉴 */}
+        <S.MenuContainer>
+          <S.MenuList>
+            {MENU_LINK.map((menu, index) => (
+              <S.MenuListItem key={index} onClick={() => navigate(menu.path)}>
+                {menu.name}
+              </S.MenuListItem>
+            ))}
+          </S.MenuList>
         </S.MenuContainer>
 
+        {/* 메뉴 버튼 */}
+        <S.UserActionsContainer>
+          {isLogin ? (
+            <>
+              <S.UserProfile>
+                <img src="https://via.placeholder.com/48" alt="user profile" />
+                박지현&nbsp;님
+              </S.UserProfile>
+              <S.MenuActionContainer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
+              </S.MenuActionContainer>
+            </>
+          ) : (
+            <>
+              <S.MenuActionContainer onClick={() => navigate('/signin')}>
+                로그인
+              </S.MenuActionContainer>
+            </>
+          )}
+        </S.UserActionsContainer>
+
+        {/* 사이드바 */}
         {isMenuOpen && <Sidebar />}
       </S.HeaderInner>
     </S.HeaderContainer>
--- src/components/common/Header/styles.ts
+++ src/components/common/Header/styles.ts
@@ -19,27 +19,65 @@ export const HeaderInner = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
-  max-width: calc(588px + 16px * 2);
+  max-width: calc(1200px + 16px * 2);
   height: 100%;
   padding: 0 16px;
 
-  .logo {
-    cursor: pointer;
+  ${theme.typography.link.l2};
 
-    @media (max-width: 500px) {
-      width: 120px;
-    }
+  .logo {
+    width: 120px;
+    min-width: 120px;
+    cursor: pointer;
   }
 `;
 
 export const MenuContainer = styled.div`
+  margin-left: ${theme.gap.gap3};
+
+  @media (max-width: 719px) {
+    display: none;
+  }
+`;
+
+export const MenuList = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 0 ${theme.gap.gap3};
+`;
+export const MenuListItem = styled.li`
+  cursor: pointer;
+`;
+
+// 유저 프로필, 메뉴 액션 컨테이너를 감싸는 컨테이너
+export const UserActionsContainer = styled.div`
+  display: inline-flex;
+  align-items: center;
+  margin-left: auto;
+`;
+
+export const UserProfile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0 ${theme.gap.gap1};
+  padding: 0 ${theme.gap.gap1};
+  cursor: pointer;
+
+  img {
+    width: 24px;
+    height: 24px;
+    object-fit: cover;
+  }
+`;
+
+export const MenuActionContainer = styled.div`
   display: inline-flex;
   align-items: center;
   justify-content: center;
   width: 48px;
   min-width: 48px;
   height: 48px;
-  margin-left: auto;
+  cursor: pointer;
 
   @media (max-width: 500px) {
     width: 40px;
