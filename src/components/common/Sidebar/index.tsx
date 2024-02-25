6526c2fe16d71bbaa22dbb4a25e496cf70c69ae2
geniee1220 <geniee1220@gmail.com>
2024-02-25 11:22:08 +0900

    Refactor : 링크 path, name을 상수 파일로 분리

--- src/components/common/Sidebar/index.tsx
+++ src/components/common/Sidebar/index.tsx
@@ -1,26 +1,12 @@
 import * as S from './styles';
-
-const SIDE_MENU_LIST = [
-  {
-    name: '현장목록',
-    path: '/remodeling/list',
-  },
-  {
-    name: '메세지',
-    path: '/message',
-  },
-  {
-    name: '설정',
-    path: '/setting',
-  },
-];
+import { SIDE_MENU_LINK } from '@/constants/link';
 
 function Sidebar() {
   return (
     <S.SideMenu>
       <nav>
         <S.SideMenuList>
-          {SIDE_MENU_LIST.map((item, index) => (
+          {SIDE_MENU_LINK.map((item, index) => (
             <S.SideMenuItem key={index}>{item.name}</S.SideMenuItem>
           ))}
         </S.SideMenuList>
