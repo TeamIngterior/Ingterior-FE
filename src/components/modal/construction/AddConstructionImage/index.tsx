99aee0e96cec5450fdb817faea8e1fb04229b670
geniee1220 <geniee1220@gmail.com>
2024-02-25 22:45:02 +0900

    Refactor : OL 라이브러리 관련 코드 삭제
    해당 모달에서는 OL 사용하지 않음

--- src/components/modal/construction/AddConstructionImage/index.tsx
+++ src/components/modal/construction/AddConstructionImage/index.tsx
@@ -1,50 +1,16 @@
-import { useRef, useEffect, useState } from 'react';
-
 import Button from '@/components/common/Button';
 import DefaultModal from '../../DefaultModal';
 
 import * as S from './styles';
 import * as CS from '@/components/modal/styles';
 
-// import Map from 'ol/Map';
-// import View from 'ol/View';
-// import TileLayer from 'ol/layer/Tile';
-// import XYZ from 'ol/source/XYZ';
-
-import 'ol/ol.css'; //스타일
-import { Map as OlMap, View } from 'ol'; //뷰 관리
-import { fromLonLat, get as getProjection } from 'ol/proj'; //위경도
-import { Tile as TileLayer } from 'ol/layer'; //지도타일
-import { OSM } from 'ol/source'; //지도정보
-
 function AddConstructionImage() {
-  const [mapObject, setMapObject] = useState({});
-
-  useEffect(() => {
-    const map = new OlMap({
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      target: 'map',
-      view: new View({
-        projection: 'EPSG:3857',
-        center: fromLonLat([126.752, 37.4713], 'EPSG:3857'),
-        zoom: 13,
-      }),
-    });
-    setMapObject({ map });
-  }, []);
-
   return (
     <DefaultModal name="addConstructionImage" className="addConstructionImage">
       {/* 모달 컨텐츠 */}
       <CS.ModalContentContainer>
         <CS.ModalHeader>영역 안에 이미지를 맞춰주세요.</CS.ModalHeader>
-        <CS.ModalContent>
-          <div id="map" style={{ height: '50rem' }} />
-        </CS.ModalContent>
+        <CS.ModalContent></CS.ModalContent>
 
         <CS.ModalButtonContainer>
           <Button size="lg" $fullWidth={true}>
