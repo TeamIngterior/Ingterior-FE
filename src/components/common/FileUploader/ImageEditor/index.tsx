257c27475d20c54f9616877bf6036d8027b684d1
geniee1220 <geniee1220@gmail.com>
2024-02-27 20:22:36 +0900

    Refactor : 조건문 리팩토링

--- src/components/common/FileUploader/ImageEditor/index.tsx
+++ src/components/common/FileUploader/ImageEditor/index.tsx
@@ -24,33 +24,42 @@ function ImageEditor({
     onImageChange(editedImage);
   });
 
+  // 편집 기능 O 단일 이미지 업로드
+  const renderEditorButtons = () => (
+    <div>
+      <button onClick={rotate}>Rotate</button>
+      <button onClick={flipHorizontally}>Flip Horizontally</button>
+      <button onClick={flipVertically}>Flip Vertically</button>
+      <button onClick={resetChanges}>Reset Changes</button>
+      <button onClick={applyChanges}>Apply Changes</button>
+      <button onClick={deleteImage}>Delete Image</button>
+      <br />
+      <canvas
+        ref={canvasRef}
+        style={{
+          maxWidth: '100%',
+          height: 'auto',
+          border: '1px solid black',
+        }}
+      />
+    </div>
+  );
+
+  // 편집 기능 X 단일 이미지 업로드
+  const renderImageView = () => (
+    <div>
+      <img src={image || editedImage} alt="Thumbnail" />
+      <button onClick={changeImage}>Change Image</button>
+    </div>
+  );
+
   return (
     <div>
       <div {...getRootProps()}>
         <input {...getInputProps()} />
         <p>이미지 드래그 앤 드랍 영역</p>
       </div>
-      {image && (
-        <div>
-          <button onClick={rotate}>Rotate</button>
-          <button onClick={flipHorizontally}>Flip Horizontally</button>
-          <button onClick={flipVertically}>Flip Vertically</button>
-          <button onClick={resetChanges}>Reset Changes</button>
-
-          <button onClick={changeImage}>Chane Image</button>
-          <button onClick={applyChanges}>Apply Changes</button>
-          <button onClick={deleteImage}>Delete Image</button>
-          <br />
-          <canvas
-            ref={canvasRef}
-            style={{
-              maxWidth: '100%',
-              height: 'auto',
-              border: '1px solid black',
-            }}
-          />
-        </div>
-      )}
+      {image && <>{isEditor ? renderEditorButtons() : renderImageView()}</>}
     </div>
   );
 }
