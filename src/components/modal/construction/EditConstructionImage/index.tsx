bd981bcf901018a7d07fb916756d8a0bef1f652a
geniee1220 <geniee1220@gmail.com>
2024-02-27 19:30:55 +0900

    Chore : Add -> Edit로 모달 이름 변경

--- src/components/modal/construction/EditConstructionImage/index.tsx
+++ src/components/modal/construction/EditConstructionImage/index.tsx
@@ -5,14 +5,16 @@ import * as S from './styles';
 import * as CS from '@/components/modal/styles';
 import ImageEditor from '@/components/common/ImageEditor';
 
-function AddConstructionImage() {
+function EditConstructionImage() {
   const handleImageChange = (editedImage: string | null) => {
-    // 이 함수에서 editedImage를 처리하거나 다른 작업을 수행합니다.
-    console.log('Edited image:', editedImage);
+    //    수정된 이미지 데이터를 받아서 처리하는 로직
   };
 
   return (
-    <DefaultModal name="addConstructionImage" className="addConstructionImage">
+    <DefaultModal
+      name="editConstructionImage"
+      className="editConstructionImage"
+    >
       {/* 모달 컨텐츠 */}
       <CS.ModalContentContainer>
         <CS.ModalHeader>영역 안에 이미지를 맞춰주세요.</CS.ModalHeader>
@@ -30,4 +32,4 @@ function AddConstructionImage() {
   );
 }
 
-export default AddConstructionImage;
+export default EditConstructionImage;
