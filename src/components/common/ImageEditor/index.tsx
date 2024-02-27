d77db93ea443614d4881586bc7b8321221f5dfc8
geniee1220 <geniee1220@gmail.com>
2024-02-27 19:31:11 +0900

    Feat : 이미지 에디터 커스텀 훅으로 변경

--- src/components/common/ImageEditor/index.tsx
+++ src/components/common/ImageEditor/index.tsx
@@ -1,79 +1,24 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { useDropzone } from 'react-dropzone';
+import { useImageEdit } from '@/hooks/useImageEdit';
 
 function ImageEditor({
   onImageChange,
 }: {
   onImageChange: (editedImage: string) => void;
 }) {
-  const [image, setImage] = useState<any>(null);
-  const [editedImage, setEditedImage] = useState<string | null>(null);
-  const [rotation, setRotation] = useState(0);
-  const [flippedHorizontally, setFlippedHorizontally] = useState(false);
-  const [flippedVertically, setFlippedVertically] = useState(false);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    if (canvasRef.current && image) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-
-      const img = new Image();
-      img.src = image;
-      img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx?.save();
-        ctx?.clearRect(0, 0, canvas.width, canvas.height);
-        ctx?.translate(canvas.width / 2, canvas.height / 2);
-        ctx?.rotate((rotation * Math.PI) / 180);
-        if (flippedHorizontally) ctx?.scale(-1, 1);
-        if (flippedVertically) ctx?.scale(1, -1);
-        ctx?.drawImage(img, -img.width / 2, -img.height / 2);
-        ctx?.restore();
-        setEditedImage(canvas.toDataURL());
-      };
-    }
-  }, [image, rotation, flippedHorizontally, flippedVertically]);
-
-  const onDrop = (acceptedFiles: any[]) => {
-    const file = acceptedFiles[0];
-    const reader = new FileReader();
-
-    reader.onload = () => {
-      setImage(reader.result);
-    };
-
-    reader.readAsDataURL(file);
-  };
-
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
-
-  const rotate = () => {
-    setRotation((prevRotation) => prevRotation + 90);
-  };
-
-  const flipHorizontally = () => {
-    setFlippedHorizontally(!flippedHorizontally);
-  };
-
-  const flipVertically = () => {
-    setFlippedVertically(!flippedVertically);
-  };
-
-  const resetChanges = () => {
-    setRotation(0);
-    setFlippedHorizontally(false);
-    setFlippedVertically(false);
-    setEditedImage(null);
-  };
-
-  const applyChanges = () => {
-    if (editedImage) {
-      console.log(editedImage);
-      onImageChange(editedImage);
-    }
-  };
+  const {
+    image,
+    getRootProps,
+    getInputProps,
+    rotate,
+    flipHorizontally,
+    flipVertically,
+    resetChanges,
+    editedImage,
+    canvasRef,
+    applyChanges,
+  } = useImageEdit((editedImage: string) => {
+    onImageChange(editedImage);
+  });
 
   return (
     <div>
