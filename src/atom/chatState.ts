3f76f6257aa8d7fa14c5c14a001e6de82defb460
geniee1220 <geniee1220@gmail.com>
2024-03-10 23:01:50 +0900

    Feat : 채팅 사이드바 Recoil State 추가

--- src/atom/chatState.ts
+++ src/atom/chatState.ts
@@ -4,3 +4,13 @@ export const chatRoomIdState = atom<null | number>({
   key: 'chatRoomIdState',
   default: null,
 });
+
+export const chatSidebarState = atom<boolean>({
+  key: 'chatSidebarState',
+  default: false,
+});
+
+export const chatSidebarInfoState = atom<any>({
+  key: 'chatSidebarInfoState',
+  default: [],
+});
