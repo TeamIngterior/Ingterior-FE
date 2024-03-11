cbe8b04a0d4ec7736c332acbacc02fd9a86c413f
geniee1220 <geniee1220@gmail.com>
2024-03-11 17:36:27 +0900

    Feat : 채팅 아이콘은 현장 추가, 하자 추가, 공사 추가 화면에서 보이지 않게 처리

--- src/components/template/MainTemplate.tsx
+++ src/components/template/MainTemplate.tsx
@@ -1,17 +1,32 @@
+import { useLocation } from 'react-router-dom';
 import Chatting from '../chat';
 
 import * as S from './styles';
+import { useEffect, useState } from 'react';
 
 interface MainTemplateProps {
   children: React.ReactNode;
 }
 
 function MainTemplate({ children }: MainTemplateProps) {
+  const { pathname } = useLocation();
+  const [isChatting, setIsChatting] = useState<boolean>(true);
+
+  useEffect(() => {
+    // 채팅은 현장추가, 하자추가, 공사추가 화면에서는 보이지 않게 처리
+    if (pathname.includes('addition') || pathname.includes('defect/addition')) {
+      setIsChatting(false);
+    } else {
+      setIsChatting(true);
+    }
+  }, [pathname]);
+
   return (
     <S.TemplateContainer>
       <S.TemplateInner>{children}</S.TemplateInner>
-      {/* 채팅(메세지) */}
-      <Chatting />
+
+      {/** 채팅(메세지) */}
+      {isChatting && <Chatting />}
     </S.TemplateContainer>
   );
 }
