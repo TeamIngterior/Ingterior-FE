6b1d8ce4610b06713cf9ba1e5edeb8dd9d65ad0f
geniee1220 <geniee1220@gmail.com>
2024-03-18 22:38:39 +0900

    Refactor : 날짜, 스케쥴 recoil atom 생성

--- src/atom/calendarState.ts
+++ src/atom/calendarState.ts
@@ -4,3 +4,13 @@ export const calendarInfoState = atom({
   key: 'calendarInfoState',
   default: false,
 });
+
+export const calendarDateState = atom<Date | null>({
+  key: 'calendarDateState',
+  default: null,
+});
+
+export const calendarScheduleListState = atom<any[]>({
+  key: 'calendarScheduleListState',
+  default: [],
+});
