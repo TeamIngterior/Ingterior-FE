e1360b88c4d81bf6cc7a69fbe3b724fb3a226123
geniee1220 <geniee1220@gmail.com>
2024-03-18 22:06:31 +0900

    Feat : 캘린더 커스텀 훅 생성

---
+++ src/hooks/useCalendar.tsx
@@ -0,0 +1,53 @@
+import EventContent from '@/components/common/calendar/EventContent';
+import { DatesSetArg, EventContentArg } from '@fullcalendar/core';
+
+import { useState } from 'react';
+
+interface IGetScheduleForm {
+  startDate: null | Date;
+  endDate: null | Date;
+}
+
+export const useCalendar = (scheduleList: any[]) => {
+  const [period, setPeriod] = useState<IGetScheduleForm>({
+    startDate: null,
+    endDate: null,
+  });
+
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [isOpenScheduleDialog, setIsOpenScheduleDialog] = useState(false);
+
+  const onDateClick = ({ date }: any) => {
+    onOpenScheduleDialog(date);
+  };
+
+  const onChangeDate = ({ startStr, endStr }: DatesSetArg) => {
+    setPeriod({ startDate: new Date(startStr), endDate: new Date(endStr) });
+  };
+
+  const onOpenScheduleDialog = (selected: Date) => {
+    setSelectedDate(selected);
+    setIsOpenScheduleDialog(true);
+  };
+
+  const eventContent = (eventInfo: EventContentArg) => {
+    return (
+      <EventContent
+        colorIndex={scheduleList.findIndex(
+          (schedule) => schedule.title === eventInfo.event.title
+        )}
+        eventInfo={eventInfo}
+      />
+    );
+  };
+
+  return {
+    onDateClick,
+    onChangeDate,
+    eventContent,
+    period,
+    selectedDate,
+    isOpenScheduleDialog,
+    setIsOpenScheduleDialog,
+  };
+};
