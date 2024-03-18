69daa44c6e6f8467b25fc920fef9812aef8296c8
geniee1220 <geniee1220@gmail.com>
2024-03-18 12:07:55 +0900

    Refactor : 하자체크 컴포넌트로 분리

---
+++ src/components/construction/detail/DetailDefact/index.tsx
@@ -0,0 +1,102 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { FiPlus } from 'react-icons/fi';
+import ConstructionListCard from '@/components/construction/list/ConstructionListCard';
+import Button from '@/components/common/Button';
+
+import * as CS from '../styles';
+import * as S from './styles';
+
+function DetailDefact() {
+  const { pathname } = useLocation();
+  const id = pathname.split('/').pop();
+  const navigate = useNavigate();
+
+  const defectList = [];
+
+  return (
+    <>
+      {/* 버튼 */}
+      <CS.DetailConstructionFunctionContainer>
+        <Button
+          type="button"
+          size="md"
+          className="addDefectButton"
+          onClickHandler={() => navigate(`/construction/defect/addition/${id}`)}
+        >
+          <FiPlus />새 하자 추가
+        </Button>
+      </CS.DetailConstructionFunctionContainer>
+
+      {/* 캔버스 영역 */}
+      <S.CanvasContainer></S.CanvasContainer>
+
+      {/* 하자 리스트 */}
+      <S.DefectListContainer>
+        {defectList.length !== 0 ? (
+          <></>
+        ) : (
+          <S.NoDefectContainer>
+            새로운 하자를 추가해 보세요!
+          </S.NoDefectContainer>
+        )}
+      </S.DefectListContainer>
+
+      {/* 현장 정보 */}
+      <S.DetailConstructionInfoContainer>
+        <S.DetailConstructionInfoTitle>현장 정보</S.DetailConstructionInfoTitle>
+
+        {/* 현장 정보 카드 */}
+        <ConstructionListCard
+          type="card"
+          cardData={{
+            id: 1,
+            title: '현장명',
+            category: ['카테고리1', '카테고리2'],
+            isOwner: true,
+            user: {
+              profileImg: 'https://via.placeholder.com/150',
+              usercode: 'usercode',
+            },
+            constructionSiteCode: 'code',
+            createdAt: '2024-03-01',
+          }}
+        />
+
+        {/* 참여자 목록 */}
+        <S.DetailConstructionMebmerContainer>
+          <S.MemberListHeader>현장 참여자</S.MemberListHeader>
+
+          <S.MemberList>
+            <S.MemberListItem>
+              {/* 프로필 이미지  */}
+              <S.MemberProfileImage>
+                <img
+                  src="https://via.placeholder.com/150"
+                  alt="프로필 이미지"
+                />
+              </S.MemberProfileImage>
+              {/* 참여자 정보 */}
+              1GA001
+            </S.MemberListItem>
+          </S.MemberList>
+        </S.DetailConstructionMebmerContainer>
+
+        {/* 하자 체크 리스트 다운 / 현장 메세지 바로가기 / 현장 나가기 버튼 */}
+        <S.DetailConstructionButtonContainer>
+          <Button type="button" $styleType="revert" $fullWidth={true}>
+            하자체크 리스트 다운받기
+          </Button>
+          <Button type="button" $styleType="revert" $fullWidth={true}>
+            현장 메세지 바로가기
+          </Button>
+          <Button type="button" $styleType="revert" $fullWidth={true}>
+            현장 나가기
+          </Button>
+        </S.DetailConstructionButtonContainer>
+      </S.DetailConstructionInfoContainer>
+    </>
+  );
+}
+
+export default DetailDefact;
