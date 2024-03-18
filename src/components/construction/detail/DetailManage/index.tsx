b4f2ff91fee7d878200f50614794a3ec94137c8d
geniee1220 <geniee1220@gmail.com>
2024-03-18 22:07:14 +0900

    Feat : 공사관리 탭 추가

--- src/components/construction/detail/DetailManage/index.tsx
+++ src/components/construction/detail/DetailManage/index.tsx
@@ -5,6 +5,7 @@ import Button from '@/components/common/Button';
 
 import * as CS from '../styles';
 import * as S from './styles';
+import Calendar from '@/components/common/calendar/Calendar';
 
 function DetailManage() {
   const { pathname } = useLocation();
@@ -13,16 +14,26 @@ function DetailManage() {
 
   return (
     <>
-      <CS.DetailConstructionFunctionContainer>
-        <Button
-          type="button"
-          size="md"
-          className="addDefectButton"
-          onClickHandler={() => navigate(`/construction/work/addition/${id}`)}
-        >
-          <FiPlus />새 공사 추가
-        </Button>
-      </CS.DetailConstructionFunctionContainer>
+      <S.DetailManageContent>
+        <CS.DetailConstructionFunctionContainer>
+          <Button
+            type="button"
+            size="md"
+            className="addDefectButton"
+            onClickHandler={() => navigate(`/construction/work/addition/${id}`)}
+          >
+            <FiPlus />새 공사 추가
+          </Button>
+        </CS.DetailConstructionFunctionContainer>
+
+        {/* 달력 */}
+        <Calendar />
+      </S.DetailManageContent>
+
+      {/* 일정 디테일 */}
+      <S.DetailManageContainer>
+        <S.DetailManageContent></S.DetailManageContent>
+      </S.DetailManageContainer>
     </>
   );
 }
--- src/components/construction/detail/DetailManage/styles.ts
+++ src/components/construction/detail/DetailManage/styles.ts
@@ -1,2 +1,16 @@
 import styled from 'styled-components';
 import { theme } from '@/assets/styles/theme';
+
+export const DetailManageContainer = styled.div`
+  padding: ${theme.gap.gap3} 0;
+  background-color: ${theme.color.gray02};
+  margin-top: ${theme.gap.gap3};
+`;
+
+export const DetailManageContent = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: calc(588px + 16px * 2);
+  padding: 0 16px;
+  margin: 0 auto;
+`;
