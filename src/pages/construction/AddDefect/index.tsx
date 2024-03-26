5b2aaff3dafe02ef33a95d53b44cea048eee7267
geniee1220 <geniee1220@gmail.com>
2024-03-26 19:35:17 +0900

    Refactor : Ol 라이브러리 대체

--- src/pages/construction/AddDefect/index.tsx
+++ src/pages/construction/AddDefect/index.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
-import ReactDOMServer from 'react-dom/server';
+import { useEffect, useState } from 'react';
 
 import { Controller, useForm } from 'react-hook-form';
 
@@ -8,18 +7,14 @@ import Input from '@/components/common/Input';
 import Button from '@/components/common/Button';
 import Marker from '@assets/marker_default.svg?react';
 
+import Canvas from '@/components/common/Canvas';
+
 import * as S from './styles';
 import * as CS from '@components/template/styles';
 import * as IS from '@components/common/Input/styles';
 import * as IES from '@components/common/FileUploader/ImageEditor/styles';
 
 import MultiUploader from '@/components/common/FileUploader/MultiUploader';
-
-import { Map as OLMap, View } from 'ol';
-import { Image as ImageLayer } from 'ol/layer';
-import { ImageStatic as ImageStaticSource } from 'ol/source';
-import Overlay from 'ol/Overlay';
-import { useTheme } from 'styled-components';
 
 interface Marker {
   id: number;
@@ -54,12 +49,6 @@ const DEFECT_ADDITION_NAV = [
 
 function AddDefect() {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const [markers, setMarkers] = useState<Overlay[]>([]);
-  const [cooridnates, setCoordinates] = useState<[number, number][]>([]);
-  const [tooltipAdded, setTooltipAdded] = useState(false);
-
-  // styled-components 의 theme를 사용할 수 있는 훅
-  const theme = useTheme();
 
   const {
     register,
@@ -84,189 +73,6 @@ function AddDefect() {
     console.log('하자 추가', data);
   };
 
-  const mapContainerRef = useRef<HTMLDivElement>(null);
-  const mapRef = useRef<OLMap | null>(null);
-  const overlayRef = useRef<Overlay | null>(null);
-  const imageUrl = 'https://via.placeholder.com/588';
-
-  useEffect(() => {
-    if (!mapContainerRef.current) return;
-
-    let overlay = overlayRef.current;
-
-    if (!overlay) {
-      overlay = new Overlay({
-        element: document.createElement('div'),
-      });
-      overlayRef.current = overlay;
-      mapRef.current?.addOverlay(overlay);
-    }
-
-    const map = new OLMap({
-      target: mapContainerRef.current,
-      layers: [
-        new ImageLayer({
-          source: new ImageStaticSource({
-            url: imageUrl,
-            projection: 'EPSG:3857',
-            imageExtent: [0, 0, 500, 500],
-          }),
-        }),
-      ],
-      view: new View({
-        zoom: 2,
-        center: [250, 250],
-        extent: [0, 0, 500, 500],
-      }),
-      interactions: [],
-      controls: [],
-    });
-
-    const handleClick = (event: any) => {
-      const clickedCoord = event.coordinate;
-      addMarker(clickedCoord);
-    };
-
-    map.on('click', handleClick);
-
-    mapRef.current = map;
-
-    return () => {
-      if (mapRef.current && overlayRef.current) {
-        mapRef.current.removeOverlay(overlayRef.current);
-        mapRef.current.un('click', handleClick);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log('coordinates', cooridnates);
-  }, [cooridnates]);
-
-  useEffect(() => {
-    console.log('markers', markers);
-
-    if (markers.length > 0 && !tooltipAdded) {
-      // 첫 번째 마커
-      const firstMarker = markers[0];
-
-      const markerElement = document.createElement('div');
-      markerElement.className = 'marker-wrapper';
-      markerElement.innerHTML = ReactDOMServer.renderToString(<Marker />);
-      markerElement.style.width = '48px';
-      markerElement.style.height = '48px';
-
-      const markerIndex = 1;
-
-      // 툴팁이 추가된 적이 없을 때만 툴팁 추가
-      if (!firstMarker.get('tooltipAdded')) {
-        const tooltipElement = document.createElement('div');
-        tooltipElement.className = 'tooltip';
-        tooltipElement.style.position = 'absolute';
-        tooltipElement.style.width = 'fit-content';
-        tooltipElement.style.whiteSpace = 'nowrap';
-        tooltipElement.style.padding = '8px 16px';
-        tooltipElement.style.backgroundColor = theme.color.secondary01;
-        tooltipElement.textContent = '마커를 하자의 위치로 드래그하세요';
-
-        // 툴팁 닫기 버튼 추가
-        const closeButton = document.createElement('span');
-        closeButton.className = 'close';
-        closeButton.textContent = 'x';
-        closeButton.addEventListener('click', () => {
-          tooltipElement.style.display = 'none';
-        });
-        tooltipElement.appendChild(closeButton);
-
-        // 툴팁 클릭 시 닫기
-        tooltipElement.addEventListener('click', () => {
-          tooltipElement.style.display = 'none';
-        });
-
-        // 마커 기준으로 중앙 정렬
-        const tooltipOverlay = new Overlay({
-          element: tooltipElement,
-          position: firstMarker.getPosition(),
-          offset: [-100, 60],
-        });
-        mapRef.current?.addOverlay(tooltipOverlay);
-
-        // 마커의 위치가 변경이 되면 툴팁 삭제
-        firstMarker.on('change:position', () => {
-          tooltipOverlay.setPosition(undefined);
-        });
-
-        // 툴팁이 추가되었음을 표시
-        firstMarker.set('tooltipAdded', true);
-
-        setTooltipAdded(true);
-      }
-    }
-  }, [markers]);
-
-  const addMarker = (coord: [number, number]) => {
-    if (overlayRef.current) {
-      console.log('coord', coord);
-
-      const markerElement = document.createElement('div');
-      markerElement.className = 'marker-wrapper';
-      markerElement.innerHTML = ReactDOMServer.renderToString(<Marker />);
-      markerElement.style.width = '48px';
-      markerElement.style.height = '48px';
-
-      overlayRef.current.setPosition(coord);
-
-      const offsetX = -24;
-      const offsetY = -24;
-      const position = [coord[0] + offsetX, coord[1] - offsetY];
-
-      if (mapRef.current) {
-        mapRef.current.getOverlays().clear();
-        setMarkers([]);
-
-        const marker = new Overlay({
-          position: position,
-          element: markerElement,
-          stopEvent: false,
-        });
-
-        setCoordinates([coord]);
-        setMarkers([marker]);
-
-        mapRef.current.addOverlay(marker);
-
-        markerElement.addEventListener('mousedown', (event) => {
-          let isDragging = false;
-          const startPosition = [event.clientX, event.clientY];
-          const startMarkerPosition = marker.getPosition() || [0, 0];
-
-          const onMouseMove = (event: MouseEvent) => {
-            const dx = event.clientX - startPosition[0];
-            const dy = event.clientY - startPosition[1];
-            const newPosition = [
-              startMarkerPosition[0] + dx,
-              startMarkerPosition[1] - dy,
-            ];
-            marker.setPosition(newPosition);
-            setCoordinates([[...coord]]);
-            isDragging = true;
-          };
-
-          const onMouseUp = () => {
-            document.removeEventListener('mousemove', onMouseMove);
-            document.removeEventListener('mouseup', onMouseUp);
-            if (isDragging) {
-              console.log('마커 위치 변경 완료:', marker.getPosition());
-            }
-          };
-
-          document.addEventListener('mousemove', onMouseMove);
-          document.addEventListener('mouseup', onMouseUp);
-        });
-      }
-    }
-  };
-
   return (
     <S.AddDefectContainer>
       <CS.TemplateTitle>하자 체크</CS.TemplateTitle>
@@ -278,7 +84,7 @@ function AddDefect() {
         {/* 컨텐츠 */}
         <S.AddDefectContent>
           {/* 캔버스 영역 */}
-          <S.CanvasContainer ref={mapContainerRef} />
+          <Canvas />
 
           {/* 인풋 영역  */}
           <S.AddDefectInputContainer>
