dd543dfbf31b81ccd51f9b1d39ace80702eca56a
geniee1220 <geniee1220@gmail.com>
2024-03-31 18:26:58 +0900

    Refactor : 간편 견적 옵션을 상수 파일로 분리

---
+++ src/constants/estimateOption.ts
@@ -0,0 +1,48 @@
+export const SASH_OPTIONS = [
+  {
+    id: 1,
+    label: '네',
+  },
+  {
+    id: 2,
+    label: '아니오',
+  },
+];
+
+export const BALCONY_OPTIONS = [
+  {
+    id: 1,
+    label: 'X',
+  },
+  {
+    id: 2,
+    label: '1',
+  },
+  {
+    id: 3,
+    label: '2',
+  },
+  {
+    id: 4,
+    label: '3',
+  },
+  {
+    id: 5,
+    label: '4',
+  },
+];
+
+export const BATHROOM_OPTIONS = [
+  {
+    id: 1,
+    label: '1',
+  },
+  {
+    id: 2,
+    label: '2',
+  },
+  {
+    id: 3,
+    label: '3',
+  },
+];
