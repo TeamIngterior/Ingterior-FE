0b0e7f68fd3950a39ff9049542a6b5278a5f389c
geniee1220 <geniee1220@gmail.com>
2024-03-31 10:07:30 +0900

    Chore : 사용하지 않는 코드 삭제

--- src/components/TestComponent/Component.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import TestComponent from '.';
-
-test('페이지 렌더링 테스트', async () => {
-  render(<TestComponent />);
-  const testButton = await screen.findByRole('button');
-  expect(testButton.innerHTML).toBe('페이지 렌더링 테스트');
-});
--- src/components/TestComponent/index.tsx
+++
@@ -1,5 +0,0 @@
-function ComponentT() {
-  return <div>테스트 컴포넌트</div>;
-}
-
-export default ComponentT;
