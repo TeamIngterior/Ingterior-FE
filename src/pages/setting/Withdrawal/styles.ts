0485de71c11fff96eb04f591a1339e382647bfc0
geniee1220 <geniee1220@gmail.com>
2024-04-13 23:30:58 +0900

    Feat : 회원탈퇴 페이지 생성

---
+++ src/pages/setting/Withdrawal/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { theme } from '@/assets/styles/theme';
+
+export const WithdrawalContainer = styled.div``;
+
+export const WithdrawalForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 40px 0;
+  margin-top: ${theme.gap.gap7};
+`;
