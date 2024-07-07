19c8646ca7e42ded0dcca031ecd0623f2f976a0e
geniee1220 <geniee1220@gmail.com>
2024-07-07 17:30:45 +0900

    Feat : 로그인 params 저장

--- src/pages/user/LoginAuth/index.tsx
+++ src/pages/user/LoginAuth/index.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 function LoginAuth() {
-  return <div>로그인 페이지입니다</div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    console.log('로그인 페이지');
+    // accessToken, refreshToken을 로컬 스토리지에 저장
+    const accessToken = searchParams.get('accessToken');
+    const refreshToken = searchParams.get('refreshToken');
+
+    localStorage.setItem('accessToken', accessToken || '');
+    localStorage.setItem('refreshToken', refreshToken || '');
+
+    // 메인 페이지로 이동
+    navigate('/');
+  }, []);
+  return <div></div>;
 }
 
 export default LoginAuth;
